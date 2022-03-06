b9dd74cb4022ef5cbfeb4472cfb2eeb730a312c0
Sam <275519434@qq.com>
2022-03-06 17:34:20 +0800

    fix1.0

--- src/main/java/com/zmy/git/GitTest.java
+++ src/main/java/com/zmy/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("提交");
         System.out.println("切换版本");
         System.out.println("切换版本3");
+        System.out.println("hot-fix 1.0");
     }
 }
