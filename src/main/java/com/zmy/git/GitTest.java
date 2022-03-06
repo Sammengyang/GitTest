f648b4e2bfe98cb67fba01ce4996ff50c97a9235
Sam <275519434@qq.com>
2022-03-06 18:21:01 +0800

    master pushtest1.0

--- src/main/java/com/zmy/git/GitTest.java
+++ src/main/java/com/zmy/git/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("切换版本3");
         System.out.println("hot-fix 1.0");
         System.out.println("master 冲突test");
+        System.out.println("test push");
     }
 }
