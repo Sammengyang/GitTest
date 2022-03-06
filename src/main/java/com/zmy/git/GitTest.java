6061efa35f541d64a52346ffe7bea7e8929d7730
Sam <275519434@qq.com>
2022-03-06 17:09:19 +0800

    first commit

---
+++ src/main/java/com/zmy/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.zmy.git;
+
+/**
+ * @author Sam  Email:superdouble@yeah.net
+ * @Description
+ * @create 2022-03-06 16:40
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("提交");
+    }
+}
