1909245e9a3e2ad0e142a13d6028db9704204a0d
Sam <275519434@qq.com>
2022-03-06 17:17:23 +0800

    3.0

--- src/main/java/com/zmy/git/GitTest.java
+++ src/main/java/com/zmy/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("提交");
         System.out.println("切换版本");
+        System.out.println("切换版本3");
     }
 }
